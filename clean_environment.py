77ba4684bc5689b7df6c443422f18615e386130a
yzion10 <yzion10@gmail.com>
2023-06-06 15:28:42 +0300

    4 commit

--- clean_environment.py
+++ clean_environment.py
@@ -3,7 +3,7 @@ import requests
 def stopRestServer():
     msg = ''
     try:
-        requests.get('http://0.0.0.0:5000/stop_server')
+        requests.get('http://127.0.0.1:5000/stop_server')
         msg = 'Rest server stopped\n'
     except requests.exceptions.RequestException as ex:
         msg = 'rest server didnt stop because: \n' + str(ex) + '\n'
